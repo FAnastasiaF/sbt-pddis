d584ead7f4741177efd6ed06123d72d4074aa59d
fanastasiiaf <nastyainet@gmail.com>
2022-11-22 21:14:38 +0300

    allure

--- task3/app/src/test/java/AppTest.java
+++ task3/app/src/test/java/AppTest.java
@@ -3,9 +3,11 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import io.qameta.allure.junit4.DisplayName;
 
 public class AppTest {
     @Test
+    @DisplayName("create  app")
     public void testAppConstructor() {
         try {
             new App();
